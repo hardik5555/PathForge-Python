82e9993853ac30f9d7b1be0bb495b8cb75832e38
Hardik Kalra <103363985+hardik5555@users.noreply.github.com>
2024-11-12 11:16:40 +0530

    Update pathforge.py

--- pathforge.py
+++ pathforge.py
@@ -1,1 +1,18 @@
+import pygame
+import math
+from queue import PriorityQueue
 
+WIDTH = 600
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("Path-Finder")
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (0,0,0)
+BLACK = (255,255,255)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
